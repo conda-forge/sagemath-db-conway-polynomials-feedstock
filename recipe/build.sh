ad93f68d5a3c9215533b2762baad8df8561a6d03
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2017-03-05 15:24:36 +0530

    Comment build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Download the build script from sage and use it
 curl -L -O https://raw.githubusercontent.com/sagemath/sage/7.5.1/build/pkgs/conway_polynomials/spkg-install
+# Sage packages are extracted to src. Simulate that by making a symlink to current dir
 ln -s . src
 chmod +x spkg-install
 ./spkg-install
